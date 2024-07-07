da57cf5b6e91c3b790c8edde435578e3a86df79e
Destroy65 <davidgope65@gmail.com>
2024-07-07 02:25:30 +0200

    jelp

--- coms2.py
+++ coms2.py
@@ -6,7 +6,11 @@ server.bind(('0.0.0.0', 6420))
 server.listen()
 (walkie1_r, addr1) = server.accept()
 
+print(addr1)
+
 (walkie2_r, addr2) = server.accept()
+
+print(addr2)
 
 walkie1_s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 walkie1_s.connect((addr2[0], 6421))
@@ -19,4 +23,5 @@ def send(sock1, sock2):
         sock1.send(sock2.recv(100))
 
 t = Thread(target=send, args=(walkie2_s, walkie1_r))
+t.run()
 send(walkie1_s, walkie2_r)
